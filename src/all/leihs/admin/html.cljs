56e7a384a8ce34ac8e57d0c733afcf08d359daa4
Max Albrecht <max.albrecht@zhdk.ch>
2021-02-04 12:53:08 +0100

    [WIP] mfa/admin-sidebar

--- src/all/leihs/admin/html.cljs
+++ src/all/leihs/admin/html.cljs
@@ -82,364 +82,108 @@
         [:span "+"
          [:span.build build]])])])
 
-(defn sidebar-dropdown [title props children]
-  (let [aprops (apply dissoc props [:active :icon :open])
-        active? (:active props)
-        icon (:icon props)
-        open? (cond (contains? props :open) (:open props) :else true)
-        ]
-    [:li (merge aprops {:class (str "XXXc-sidebar-nav-dropdown " (when open? " XXXc-show ") (:class aprops))} )
-     [:a (merge {:class (str "XXXc-sidebar-nav-link nav-dropdown-toggle " (when active? "XXXc-active"))})
-      (when icon [:<> [:i {:class (str "XXXc-sidebar-nav-icon " icon)}] " "])
-      title]
-     (when (seq children)
-       [:ul {:class "XXXc-sidebar-nav-dropdown-items"}
-        children])]))
-  
-
-(defn sidebar-nav-item [props children]
-  (let [nprops (apply dissoc props [:href :badge :icon :active])
-        active? (:active props)
-        href (:href props)
-        icon (:icon props)
-        badge (:badge props)]
-    [:li (merge nprops {:class (str "XXXc-sidebar-nav-item " (:class nprops))})
-     [:a (merge {:class (str "XXXc-sidebar-nav-link " (when active? " XXXc-active ")) :href href})
-      (when icon [:<> [:i {:class (str "XXXc-sidebar-nav-icon " icon)}] " "])
-      children
-      (when badge badge)]]))
-
-#_(defn fake-lte-sidebar-content []
-  [:nav {:class "mt-2"}
-   [:ul {:class "nav nav-pills nav-sidebar nav-child-indent flex-column", :data-widget "treeview", :role "menu"}
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//index.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-microchip"}]
-      [:p "Installation"]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//dependencies.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-handshake"}]
-      [:p "Dependencies & Plugins"]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//layout.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-copy"}]
-      [:p "Layout"]]]
-    [:li {:class "nav-item menu-is-opening menu-open"}
-     [:a {:href "/docs/3.1//components", :class "nav-link active"}
-      [:i {:class "nav-icon fas fa-th"}]
-      [:p
-       "Components"
-       [:i {:class "right fas fa-angle-left"}]]]
-     [:ul {:class "nav nav-treeview", :style {:display "block"}}
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/main-header.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Main Header"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/main-sidebar.html", :class "nav-link active"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Main Sidebar"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/control-sidebar.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Control Sidebar"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/cards.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Card"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/boxes.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Small-/ Info-Box"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/direct-chat.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Direct Chat"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/timeline.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Timeline"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/ribbons.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Ribbons"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/miscellaneous.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Miscellaneous"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//components/plugins.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Plugins"]]]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//javascript", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-code"}]
-      [:p
-       [:i {:class "right fas fa-angle-left"}]]]
-     [:ul {:class "nav nav-treeview"}
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/layout.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Layout"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/push-menu.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Push Menu"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/treeview.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Treeview"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/card-widget.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Card Widget"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/card-refresh.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "CardRefresh"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/control-sidebar.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Control Sidebar"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/direct-chat.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Direct Chat"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/todo-list.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Todo List"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/toasts.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Toasts"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/sidebar-search.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Sidebar Search"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/expandable-tables.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "Expandable Tables"]]]
-      [:li {:class "nav-item"}
-       [:a {:href "/docs/3.1//javascript/iframe.html", :class "nav-link"}
-        [:i {:class "far fa-circle nav-icon"}]
-        [:p "IFrame"]]]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//browser-support.html", :class "nav-link"}
-      [:i {:class "nav-icon fab fa-chrome"}]
-      [:p "Browser Support\n                "]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//implementations.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-bookmark"}]
-      [:p]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//additional-styles.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-mortar-pestle"}]
-      [:p "Additional Styles\n                "]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//upgrade-guide.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-hand-point-up"}]
-      [:p "Upgrade Guide\n                "]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//faq.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-question-circle"}]
-      [:p]]]
-    [:li {:class "nav-item"}
-     [:a {:href "/docs/3.1//license.html", :class "nav-link"}
-      [:i {:class "nav-icon fas fa-file-contract"}]
-      [:p]]]]])
-
-#_(defn main-sidebar []
-  (let [open? true]
-    [:aside {:class "main-sidebar sidebar-dark-primary XXXelevation-4"}
-     [:nav.sidebar {:class (str "sidebar " (when open? "XXXc-sidebar-show"))}
-
-      [fake-lte-sidebar-content]
-      
-      #_[:nav {:class "mt-2"}
-       [:ul {:class "nav nav-pills nav-sidebar nav-child-indent flex-column", :data-widget "treeview", :role "menu"}
-        [:li {:class "nav-item menu-is-opening menu-open"}
-         [:a {:href "/docs/3.1//components", :class "nav-link active"}
-          [:i {:class "nav-icon fas fa-th"}]
-          [:p "Components"
-           [:i {:class "right fas fa-angle-left"}]]]
-         [:ul {:class "nav nav-treeview", :style {:display "block"}}
-          
-          [breadcrumbs/li :admin-buildings " Buildings " {} {} :authorizers [auth/admin-scopes?]]
-          
-          [breadcrumbs-inventory-pools/inventory-pools-li]
-          
-          
-          #_[:li {:class "nav-item"}
-           [:a {:href "/docs/3.1//components/main-header.html", :class "nav-link"}
-            [:i {:class "far fa-circle nav-icon"}]
-            [:p "Main Header"]]]
-          ]]]]
-
-      #_[:div {:class "XXXsidebar" #_:style #_{:background "#1b261a"}}
-
-         [:ul {:class "nav flex-column mb-2"}
-
-          [:li {:class "XXXc-sidebar-nav-title bg-light text-dark"} "Manage"]
-
-          [sidebar-nav-item {:href "#", :active false, :icon "fas fa-cubes"} "Inventory-Pools"]
-    ;  [sidebar-nav-item {:href "#", :class "" :active true, :icon "fas fa-cube"} ]
-          [:li.XXXc-sidebar-nav-dropdown.XXXc-show
-           [:a (merge {:class (str "XXXc-sidebar-nav-link nav-dropdown-toggle XXXc-active font-bold")})
-            [:<> [:i {:class (str "XXXc-sidebar-nav-icon fas fa-cube")}] " "]
-            "ITZ-Ausstellungen"]
-           [:div.pl-5
-            [sidebar-nav-item {:href "#", :icon "fas fa-user-friends"} "Users"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-users"} "Groups"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-hands-helping"} "Delegations"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-hands"} "Entitlement-Groups"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Mail Templates"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Fields"]]]]]
-
-      #_[:div {:class "XXXc-sidebar XXXc-sidebar-dark XXXc-sidebar-show" #_:style #_{:background "#1b261a"}}
-
-         [:ul {:class "nav flex-column mb-2"}
-
-          [:li {:class "XXXc-sidebar-nav-title"} "Reports"]
-
-          [sidebar-nav-item {:href "#", :icon "fas fa-chart-line"} "Statistics"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-cube"} "Inventory"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-thermometer-half"} "Status Info"]
-          [sidebar-dropdown "Audits" {:icon "fas fa-history" :open false}
-           [:<>
-            [sidebar-nav-item {:href "#", :icon "fas fa-history"} "Legacy"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-save"} "Audited Changes"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-exchange-alt"} "Audited Requests"]
-            #_[sidebar-nav-item {:href "#"} " Three"]]]
-
-          [:li {:class "XXXc-sidebar-nav-title"} "Configuration"]
-
-          [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Fields"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Buildings"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Rooms"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Suppliers"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Languages"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-list"} "Mail Templates"]
-
-
-          [:li {:class "XXXc-sidebar-nav-title"} "Administration"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-user-friends"} "Users"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-users"} "Groups"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-user-astronaut"} "System-Admins"]
-          [sidebar-nav-item {:href "#", :icon "fas fa-id-card"} "Authentication-Systems"]
-
-          [sidebar-dropdown "Settings" {:open true}
-           [:<>
-            [sidebar-nav-item {:href "#", :icon "fas fa-globe"} "Languages"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-box-open"} "Miscellaneous"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-paper-plane"} "SMTP"]
-            [sidebar-nav-item {:href "#", :icon "fas fa-shield-alt"} "System & Security"]
-            #_[sidebar-nav-item {:href "#"} " Three"]]]
-
-    ; [sidebar-dropdown "A Menu" {}
-    ;  [:<>
-    ;   [sidebar-nav-item {:href "#" :badge [:span {:class "badge badge-primary"} "YO"]} "One"]
-    ;   [sidebar-nav-item {:href "#"} "Two"]
-    ;   [sidebar-nav-item {:href "#"} #_[:i {:class "XXXc-sidebar-nav-icon cil-puzzle"}] " Three"]]]
-          ]
-         [:button {:class "XXXc-sidebar-minimizer XXXc-brand-minimizer", :type "button"}]]]]))
 
 (defn fake-sidebar-menu-tree []
+  ;; `:id` is for react keys and such
+  ;; `:active` means we are on or inside this route
+  ;; :link is an object of props for `<a>` - needs at least `:href` of course
   [{:id "manage"
     :label "Manage"
     :submenu
-    [{:id "pools", :label "Inventory-Pools"}
-     {:id "aa54457f-e470-4cca-bb12-516552e98777"
+    [{:id "pools"
+      :label "Inventory-Pool"
+      :link {:href "/admin/inventory-pools/"}}
+     {:id "4a1ba40c-467e-5efe-8cf1-e8d3dbb59f04"
       :label "ITZ-Ausstellungen"
-      :icon "Icon.AdminPool"
+      :icon UI/Components.Icon.AdminPool
       :active true
+      :link {:href "/admin/inventory-pools/4a1ba40c-467e-5efe-8cf1-e8d3dbb59f04"}
       :submenu
       [{:id "users"
         :label "Users"
-        :icon "Icon.AdminUsers"}
+        :icon UI/Components.Icon.AdminUsers}
        {:id "groups"
         :label "Groups"
-        :icon "Icon.AdminGroups"
+        :icon UI/Components.Icon.AdminGroups
         :active true}
        {:id "delegations"
         :label "Delegations"
-        :icon "Icon.AdminDelegations"}
+        :icon UI/Components.Icon.AdminDelegations}
        {:id "entitlement-groups"
         :label "Entitlement-Groups"
-        :icon "Icon.AdminEntitlementGroups"}
+        :icon UI/Components.Icon.AdminEntitlementGroups}
        {:id "mail-templates"
         :label "Mail Templates"
-        :icon "Icon.AdminMenuItemSettings"}
+        :icon UI/Components.Icon.AdminMenuItemSettings}
        {:id "fields"
         :label "Fields"
-        :icon "Icon.AdminMenuItemSettings"}]}]}
+        :icon UI/Components.Icon.AdminMenuItemSettings}]}]}
    {:id "reports"
     :label "reports"
     :submenu
     [{:id "statistics"
       :label "Statistics"
-      :icon "Icon.AdminStatistics"}
+      :icon UI/Components.Icon.AdminStatistics}
      {:id "inventory"
       :label "Inventory Export"
-      :icon "Icon.AdminExportInventory"}
+      :icon UI/Components.Icon.AdminExportInventory}
      {:id "status-info"
       :label "Status Info"
-      :icon "Icon.AdminStatusInfo"}
+      :icon UI/Components.Icon.AdminStatusInfo}
      {:id "audits"
       :label "Audits"
-      :icon "Icon.AdminAudits"
+      :icon UI/Components.Icon.AdminAudits
       :submenu
       [{:id "legacy"
         :label "Legacy"
-        :icon "Icon.AdminAuditsLegacy"}
+        :icon UI/Components.Icon.AdminAuditsLegacy}
        {:id "audited-changes"
         :label "Audited Changes"
-        :icon "Icon.AdminAuditedChanges"}
+        :icon UI/Components.Icon.AdminAuditedChanges}
        {:id "audited-requests"
         :label "Audited Requests"
-        :icon "Icon.AdminAuditedRequests"}]}]}
+        :icon UI/Components.Icon.AdminAuditedRequests}]}]}
    {:id "configuration"
     :label "Configuration"
     :submenu
     [{:label "Fields"
-      :icon "Icon.AdminMenuItemSettings"}
+      :icon UI/Components.Icon.AdminMenuItemSettings}
      {:label "Buildings"
-      :icon "Icon.AdminMenuItemSettings"}
+      :icon UI/Components.Icon.AdminMenuItemSettings}
      {:label "Rooms"
-      :icon "Icon.AdminMenuItemSettings"}
+      :icon UI/Components.Icon.AdminMenuItemSettings}
      {:label "Suppliers"
-      :icon "Icon.AdminMenuItemSettings"}
+      :icon UI/Components.Icon.AdminMenuItemSettings}
      {:label "Languages"
-      :icon "Icon.AdminMenuItemSettings"}
+      :icon UI/Components.Icon.AdminMenuItemSettings}
      {:label "Mail Templates"
-      :icon "Icon.AdminMenuItemSettings"}]}
+      :icon UI/Components.Icon.AdminMenuItemSettings}]}
    {:id "administration"
     :label "Administration"
     :submenu
-    [{:label "Users", :icon "Icon.AdminUsers"}
-     {:label "Groups", :icon "Icon.AdminGroups"}
+    [{:label "Users", :icon UI/Components.Icon.AdminUsers}
+     {:label "Groups", :icon UI/Components.Icon.AdminGroups}
      {:label "System-Admins"
-      :icon "Icon.AdminSystemAdmins"}
+      :icon UI/Components.Icon.AdminSystemAdmins}
      {:label "Authentication-Systems"
-      :icon "Icon.AdminAuthSystems"}
+      :icon UI/Components.Icon.AdminAuthSystems}
      {:id "settings"
       :label "Settings"
-      :icon "Icon.AdminMenuItemSettings"
+      :icon UI/Components.Icon.AdminMenuItemSettings
       :submenu
       [{:label "Languages"
-        :icon "Icon.AdminLanguages"}
+        :icon UI/Components.Icon.AdminLanguages}
        {:label "Miscellaneous"
-        :icon "Icon.AdminSettingsMisc"}
+        :icon UI/Components.Icon.AdminSettingsMisc}
        {:label "SMTP"
-        :icon "Icon.AdminSettingsSMTP"}
+        :icon UI/Components.Icon.AdminSettingsSMTP}
        {:label "System & Security"
-        :icon
-        "Icon.AdminSettingsSystemSecurity"}]}]}]
+        :icon UI/Components.Icon.AdminSettingsSystemSecurity}]}]}]
 )
 
 (defn main-sidebar []
   [:> UI/Components.DebugProps "hoi"]
+  [:> UI/Components.Admin.Sidebar {:menuTree (clj->js (fake-sidebar-menu-tree))}]
   )
 (js/console.log UI)
 
